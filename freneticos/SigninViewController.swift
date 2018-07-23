1a307d1fe707d770484c5aa899180dbdb9929327
Pedro Kayser <pedrokayser00@gmail.com>
2018-07-23 15:00:49 -0300

    init signup

--- freneticos/SigninViewController.swift
+++ freneticos/SigninViewController.swift
@@ -2,8 +2,39 @@ import UIKit
 
 class SigninViewController: UIViewController {
     
+    // MARK: - outlets
+    
+    @IBOutlet weak var email: StandardTextField!
+    @IBOutlet weak var password: StandardTextField!
+    
+    // MARK: - cycle functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        email.delegate = self
+        password.delegate = self
+    }
+    
+    // MARK: - action functions
+    
+    @IBAction func unwindSignin(segue: UIStoryboardSegue) {}
+    
+    @IBAction func clickSingin(_ sender: StandardButton) {
+    }
+    
+    @IBAction func clickSingup(_ sender: StandardButton) {
+        performSegue(withIdentifier: "gotoSignup", sender: nil)
+    }
+    
+}
+
+extension SigninViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // drop keyboard on click return
+        textField.resignFirstResponder()
+        return true
     }
     
 }
--- freneticos/SignupViewController.swift
+++ freneticos/SignupViewController.swift
@@ -2,8 +2,43 @@ import UIKit
 
 class SignupViewController: UIViewController {
     
+    // MARK: - outlets
+    
+    @IBOutlet weak var phone: StandardTextField!
+    @IBOutlet weak var name: StandardTextField!
+    @IBOutlet weak var email: StandardTextField!
+    @IBOutlet weak var password: StandardTextField!
+    @IBOutlet weak var confirmPassword: StandardTextField!
+    
+    // MARK: - cycle functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        phone.delegate = self
+        name.delegate = self
+        email.delegate = self
+        password.delegate = self
+        confirmPassword.delegate = self
+    }
+    
+    // MARK: - action functions
+    
+    @IBAction func clickSignup(_ sender: StandardButton) {
+    }
+    
+    @IBAction func clickSignin(_ sender: StandardButton) {
+        performSegue(withIdentifier: "unwindSignin", sender: nil)
+    }
+    
+}
+
+extension SignupViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // drop keyboard on click return
+        textField.resignFirstResponder()
+        return true
     }
     
 }
