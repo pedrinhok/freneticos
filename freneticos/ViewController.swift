27f5b1a1b26e289d87b831e4acd1c8a4d5da519e
Pedro Kayser <pedrokayser00@gmail.com>
2018-07-23 12:06:42 -0300

    init signin

--- freneticos/ViewController.swift
+++ freneticos/ViewController.swift
@@ -2,8 +2,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if UserService.getCurrent() == nil {
+            performSegue(withIdentifier: "gotoSignin", sender: nil)
+        }
     }
     
 }
--- freneticos/services/UserService.swift
+++ freneticos/services/UserService.swift
@@ -1,3 +1,16 @@
 import Foundation
+import CoreData
 
-class UserService {}
+class UserService {
+    
+    public static func getCurrent() -> ManagedUser? {
+        do {
+            let request: NSFetchRequest<ManagedUser> = ManagedUser.fetchRequest()
+            let response = try AppDelegate.persistentContainer.viewContext.fetch(request)
+            return response.first
+        } catch {
+            return nil
+        }
+    }
+    
+}
