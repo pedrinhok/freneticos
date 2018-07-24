4362514739bd40f337490e24fa3b154d9625fe50
Pedro Kayser <pedrokayser00@gmail.com>
2018-07-24 20:51:56 -0300

    tabbar items

---
+++ freneticos/layouts/StandardTabBar.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class StandardTabBar: UITabBar {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        guard let items = items else { return }
+        
+        var space: CGFloat = 0.0
+        switch traitCollection.horizontalSizeClass {
+        case .compact:
+            space = 5.0
+        case .regular:
+            space = 0.0
+        default:
+            space = 0.0
+        }
+        
+        for i in items {
+            i.title = ""
+            i.imageInsets = UIEdgeInsets(top: space, left: 0.0, bottom: -space, right: 0.0)
+        }
+    }
+    
+}
