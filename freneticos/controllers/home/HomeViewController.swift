0d14628c1efd22ee73b5e4c1e74ec87eb0e7b045
Pedro Kayser <pedrokayser00@gmail.com>
2018-07-25 16:29:42 -0300

    init home

--- freneticos/controllers/home/HomeViewController.swift
+++ freneticos/controllers/home/HomeViewController.swift
@@ -1,6 +1,27 @@
 import UIKit
+import MapKit
 
 class HomeViewController: UIViewController {
+    
+    // MARK: - properties
+    
+    var locationManager = CLLocationManager()
+    
+    // MARK: - outlets
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    // MARK: - cycle functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        map.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
     
     // MARK: - action functions
     
@@ -9,5 +30,20 @@ class HomeViewController: UIViewController {
     @IBAction func clickNewMatch(_ sender: UIBarButtonItem) {
         performSegue(withIdentifier: "gotoNewMatch", sender: nil)
     }
+    
+}
+
+extension HomeViewController: CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0] as CLLocation
+        
+        let span = MKCoordinateSpanMake(0.008, 0.008)
+        let region = MKCoordinateRegion(center: location.coordinate, span: span)
+        map.setRegion(region, animated: true)
+        map.showsUserLocation = true
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {}
     
 }
