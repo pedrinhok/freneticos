061530deae04e3361a721e66379f5556c8022e60
Pedro Kayser <pedrokayser00@gmail.com>
2018-07-26 16:27:54 -0300

    create match

--- freneticos/controllers/home/SetLocationViewController.swift
+++ freneticos/controllers/home/SetLocationViewController.swift
@@ -6,6 +6,7 @@ class SetLocationViewController: UIViewController {
     // MARK: - properties
     
     var locationManager = CLLocationManager()
+    var geocoder = CLGeocoder()
     var match: Match!
     
     // MARK: - outlets
@@ -18,16 +19,48 @@ class SetLocationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        locationManager.delegate = self
-        map.delegate = self
         name.delegate = self
         
         name.text = match.location
         
-        locationManager.startUpdatingLocation()
+        setCurrentLocation()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(mapObserver))
+        map.addGestureRecognizer(tap)
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardObserver), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardObserver), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    // MARK: - functions
+    
+    func setCurrentLocation() {
+        var coordinate: CLLocationCoordinate2D
+        
+        if let x = match.x, let y = match.y {
+            coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(x), longitude: CLLocationDegrees(y))
+            setAnnotation(coordinate: coordinate)
+        } else {
+            guard let location = locationManager.location else { return }
+            coordinate = location.coordinate
+        }
+        
+        let span = MKCoordinateSpanMake(0.008, 0.008)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        map.setRegion(region, animated: true)
+    }
+    
+    func setAnnotation(coordinate: CLLocationCoordinate2D) {
+        map.removeAnnotations(map.annotations)
+        let ann = MKPointAnnotation()
+        ann.coordinate = coordinate
+        map.addAnnotation(ann)
+        
+        let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            guard let placemark = placemarks?.first else { return }
+            print("\(placemark.name ?? ""), \(placemark.locality ?? ""), \(placemark.administrativeArea ?? ""), \(placemark.country ?? "")")
+        }
     }
     
     // MARK: - actions
@@ -35,6 +68,16 @@ class SetLocationViewController: UIViewController {
     @IBAction func clickSubmit(_ sender: StandardButton) {}
     
     // MARK: - selectors
+    
+    @objc func mapObserver(tap: UITapGestureRecognizer) {
+        let position = tap.location(in: map)
+        let coordinate = map.convert(position, toCoordinateFrom: map)
+        
+        match.x = coordinate.latitude
+        match.y = coordinate.longitude
+        
+        setAnnotation(coordinate: coordinate)
+    }
     
     @objc func keyboardObserver(notification: NSNotification) {
         let frame: CGRect = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
@@ -48,7 +91,7 @@ class SetLocationViewController: UIViewController {
     
 }
 
-extension SetLocationViewController: CLLocationManagerDelegate, MKMapViewDelegate {
+extension SetLocationViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations[0] as CLLocation
@@ -58,8 +101,6 @@ extension SetLocationViewController: CLLocationManagerDelegate, MKMapViewDelegat
         map.setRegion(region, animated: true)
         map.showsUserLocation = true
     }
-    
-    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {}
     
 }
 
